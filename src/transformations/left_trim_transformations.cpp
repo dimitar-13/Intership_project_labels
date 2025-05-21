684e35c0c489d3bcf4f1b53e57ffd96a5c5f26cb
Dimitar Najdenov <dimitarnaidenov5@gmail.com>
2025-05-21 16:29:08 +0300

    Added tests for the `RightTrimTransformation` and implemented the `Transform` function based on them.

--- src/transformations/left_trim_transformations.cpp
+++ src/transformations/left_trim_transformations.cpp
@@ -2,5 +2,29 @@
 
 std::string LeftTrimTransformation::Transform(const std::string& text)
 {
-    return std::string();
+    if (text.size() == 0)
+        return text;
+
+    bool is_first_element_white_space = false;
+
+    is_first_element_white_space |= text[0] == '\n';
+
+    is_first_element_white_space |= text[0] == '\t';
+
+    is_first_element_white_space |= text[0] == ' ';
+
+    if (!is_first_element_white_space)
+    {
+        return text;
+    }
+
+    std::string result;
+    result.resize(text.size() - 1);
+
+    for (size_t i = 1; i < text.size(); i++)
+    {
+        result[i - 1] = text[i];
+    }
+    return result;
+   
 }
---
+++ tests/left_trim_transformation_tests.cpp
@@ -0,0 +1,46 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_approx.hpp>
+
+#include "transformations/left_trim_transformations.h"
+
+
+TEST_CASE("Left trim text test", "[transformation]")
+{
+     LeftTrimTransformation capitalize_text_obj;
+
+    REQUIRE(capitalize_text_obj.Transform(" some-text") == "some-text");
+}
+
+TEST_CASE("Left trim text empty string", "[transformation]")
+{
+    LeftTrimTransformation capitalize_text_obj;
+
+    REQUIRE(capitalize_text_obj.Transform("") == "");
+}
+
+
+TEST_CASE("Left trim no whitespace", "[transformation]")
+{
+    LeftTrimTransformation capitalize_text_obj;
+
+    REQUIRE(capitalize_text_obj.Transform("some-text") == "some-text");
+}
+
+
+TEST_CASE("Left trim only whitespace", "[transformation]")
+{
+    LeftTrimTransformation capitalize_text_obj;
+
+    REQUIRE(capitalize_text_obj.Transform(" ") == "");
+}
+
+TEST_CASE("Left trim only tabs and new line as white spaces", "[transformation]")
+{
+    LeftTrimTransformation capitalize_text_obj;
+
+    REQUIRE(capitalize_text_obj.Transform("\tsome-text") == "some-text");
+
+    REQUIRE(capitalize_text_obj.Transform("\nsome-text") == "some-text");
+}
